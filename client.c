de54494d86b3596298b35c0d6ece144023e6e1ce
Shaher Assi <https://github.com/ShaherAssi>
2026-08-11 19:29:17 +0300

    Implement TCP client and server communication

--- client.c
+++ client.c
@@ -1,6 +1,36 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+
+    printf("Client Starting...\n");
+
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        perror("connect");
+        return 1;
+    }
+
+    printf("Client Connected...\n");
+
+    char *msg = "GET / HTTP/1.1\r\n" "Host: localhost\r\n" "\r\n";
+
+    send(client_socket, msg, strlen(msg), 0);
+
+    char buff[1024];
+
+    int buffer_bytes = recv(client_socket, buff, sizeof(buff), 0);
+    buff[buffer_bytes] = '\0';
+
+    printf("recieved: %s\n", buff);
+
     return 0;
 }
--- server.c
+++ server.c
@@ -2,6 +2,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(void) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -16,10 +18,37 @@ int main(void) {
         return 1;
     }
 
+    printf("Server Starting...\n");
+
     if (listen(sock, 5) == -1) {
         perror("listen");
         return 1;
     }
+
+    printf("Server Listening...\n");
+
+    int client_socket = accept(sock, NULL, NULL);
+    if (client_socket == -1) {
+        perror("accept");
+        return 1;
+    }
+    
+    printf("Connected to Client...\n");
+    
+    char buffer[1024];
+
+    int bytes_buffer = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+    buffer[bytes_buffer] = '\0';
+
+    printf("recived: %s\n", buffer);
+
+    char *response = "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: 5\r\n"
+    "\r\n"
+    "Hello";
+
+    send(client_socket, response, strlen(response),0);
 
     return 0;
 }
